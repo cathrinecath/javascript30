ceaafead3bc971f426e764ddc9f3b95a136d7a3e
cathrinecath <32978512+cathrinecath@users.noreply.github.com>
2019-05-15 22:58:20 +1000

    Drum Kit

---
+++ 01 - JavaScript Drum Kit/drum-kit.js
@@ -0,0 +1,21 @@
+function playSound(e){
+	const audio = document.querySelector(`audio[data-key="${e.keyCode}"`);
+	const key = document.querySelector(`.key[data-key="${e.keyCode}"`);
+	
+	if(!audio) return; //stop the function from running al together
+	audio.currentTime = 0; //rewind to start
+
+	audio.play();
+
+	key.classList.add('playing');
+	//This is the same thing as key.addClass('playing') in jquery
+}
+
+function removeTransition(e) {
+	if(e.propertyName !== 'transform') return; //skip if it is not transform
+	this.classList.remove('playing');
+}
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+window.addEventListener('keydown', playSound);
